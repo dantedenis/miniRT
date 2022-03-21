2b70ac364a5b80aacb29e326c06faca172322351
lcoreen <lcoreen@21-school.student.ru>
2022-03-21 21:59:58 +0300

    add keys

--- includes/keys.h
+++ includes/keys.h
@@ -6,7 +6,7 @@
 /*   By: lcoreen <lcoreen@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/21 18:14:42 by lcoreen           #+#    #+#             */
-/*   Updated: 2022/03/12 12:29:17 by lcoreen          ###   ########.fr       */
+/*   Updated: 2022/03/13 19:52:15 by lcoreen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 # define ARROW_LEFT 123
 # define ARROW_DOWN 125
 # define ARROW_RIGHT 124
-# define ESC 53
+# define ESC 65307
 # define BUTTON_C 8
 # define BUTTON_PLUS 69
 # define BUTTON_MINUS 78
